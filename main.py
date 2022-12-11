04e0e0f7f9a6e19b7860ec395c8e59cdab93df24
Matveev Andrey <116314099+circlsun@users.noreply.github.com>
2022-12-11 10:30:33 +0300

    Add function save_image

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ if not os.path.isdir('images'):
     os.mkdir('images')
 
 filename = 'hubble.jpeg'
-url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-images = f'{os.getcwd()}/images/{filename}'
+url_images = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+path_images = f'{os.getcwd()}/images/{filename}'
 
-response = requests.get(url)
-response.raise_for_status()
 
-with open(images, 'wb') as file:
-    file.write(response.content)
+def save_images(url, path):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(path, 'wb') as file:
+        file.write(response.content)
+
+
+save_images(url_images, path_images)
