344ab5b58884d642327460c5b24002ccb03d4193
Matveev Andrey <circlsun@gmail.com>
2022-12-11 18:27:09 +0300

    Add fetch_spacex_last_launch

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ if not os.path.isdir('images'):
     os.mkdir('images')
 
 
-
 def save_images(url, path):
     response = requests.get(url)
     response.raise_for_status()
@@ -14,14 +13,16 @@ def save_images(url, path):
         file.write(response.content)
 
 
-id = '61eefaa89eb1064137a1bd73'
-base_url = f"https://api.spacexdata.com/v5/launches/{id}"
-response = requests.get(base_url)
-response.raise_for_status()
-images_links = response.json()["links"]['flickr']['original']
-print(images_links)
+def fetch_spacex_last_launch():
+    id = '61eefaa89eb1064137a1bd73'
+    base_url = f"https://api.spacexdata.com/v5/launches/{id}"
+    response = requests.get(base_url)
+    response.raise_for_status()
+    images_links = response.json()["links"]['flickr']['original']
 
-for image_number, images_link in enumerate(images_links):
-    filename = f'spacex{image_number}.jpg'
-    path_images = f'{os.getcwd()}/images/{filename}'
-    save_images(images_link, path_images)
+    for image_number, images_link in enumerate(images_links):
+        filename = f'spacex{image_number}.jpg'
+        path_images = f'{os.getcwd()}/images/{filename}'
+        save_images(images_link, path_images)
+
+fetch_spacex_last_launch()
