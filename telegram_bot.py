4b174dc9deaf7cdaa50d8b0d1a336313efe372c6
circlsun <circlsun@gmail.com>
2022-12-13 14:56:12 +0300

    Publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,7 @@
 import os
 import telegram
 from dotenv import load_dotenv
+from functools import wraps
 
 load_dotenv()
 TG_TOKEN = os.environ['TELEGRAM_TOKEN']
@@ -11,6 +12,6 @@ print(TG_TOKEN, TG_CHAT_ID)
 bot = telegram.Bot(token=TG_TOKEN)
 updates = bot.get_updates()
 
-bot.send_message(text='There will be a channel about space!', chat_id=TG_CHAT_ID)
+bot.send_photo(chat_id=TG_CHAT_ID, photo=open('images/nasa_apod_0.jpg', 'rb'))
 
 print(updates[0])
