85bf6cd81f8e4e0fe4f53d98f0bf3ed577cbe835
circlsun <circlsun@gmail.com>
2022-12-13 15:00:49 +0300

    Add main to telegram_bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,17 +1,16 @@
 import os
 import telegram
 from dotenv import load_dotenv
-from functools import wraps
 
-load_dotenv()
-TG_TOKEN = os.environ['TELEGRAM_TOKEN']
-TG_CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
 
-print(TG_TOKEN, TG_CHAT_ID)
+def main():
+    load_dotenv()
+    TG_TOKEN = os.environ['TELEGRAM_TOKEN']
+    TG_CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
 
-bot = telegram.Bot(token=TG_TOKEN)
-updates = bot.get_updates()
+    bot = telegram.Bot(token=TG_TOKEN)
+    bot.send_photo(chat_id=TG_CHAT_ID, photo=open('images/nasa_apod_0.jpg', 'rb'))
 
-bot.send_photo(chat_id=TG_CHAT_ID, photo=open('images/nasa_apod_0.jpg', 'rb'))
 
-print(updates[0])
+if __name__ == "__main__":
+    main()
