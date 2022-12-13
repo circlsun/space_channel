7b9a178a5fcc2abd0c83228b9c6166275d0b26ef
circlsun <circlsun@gmail.com>
2022-12-13 16:47:51 +0300

    Add publication of all photos by timer

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,15 +1,44 @@
 import os
 import telegram
 from dotenv import load_dotenv
+from pathlib import Path
+import time
+import random
+
+
+def get_list_files():
+    directory = 'images'
+    files = Path(directory).glob('*')
+    list_files = []
+    for file in files:
+        list_files.append(str(file))
+    return list_files
+
+
+def send_telegram_photo(token, chat_id, photo):
+    bot = telegram.Bot(token=token)
+    bot.send_photo(chat_id=chat_id,
+                   photo=open(photo, 'rb'))
 
 
 def main():
     load_dotenv()
-    TG_TOKEN = os.environ['TELEGRAM_TOKEN']
-    TG_CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
+    tg_token = os.environ['TELEGRAM_TOKEN']
+    tg_chat_id = os.environ['TELEGRAM_CHAT_ID']
+    friquency_publications = int(os.environ['FRIQUENCY'])
 
-    bot = telegram.Bot(token=TG_TOKEN)
-    bot.send_photo(chat_id=TG_CHAT_ID, photo=open('images/nasa_apod_0.jpg', 'rb'))
+    count = len(get_list_files())
+
+    while True:
+        print()
+        count = len(get_list_files())
+        list_photos = random.sample(get_list_files(), count)
+
+        while count > 0:
+            count -= 1
+            print(list_photos[count])
+            send_telegram_photo(tg_token, tg_chat_id, list_photos[count])
+            time.sleep(friquency_publications)
 
 
 if __name__ == "__main__":
